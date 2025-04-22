92e2621918fb7900693915d3cb10975965edd461
Fareiltchemwe <tchemwew@gmail.com>
2025-04-22 15:53:40 +0200

    fixed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -24,6 +24,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
 });
 
 db.connect((err) => {
